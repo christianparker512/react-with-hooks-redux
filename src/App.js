a762a4356cba61694c7ce3e657f034fa8712fd8d
Christian Parker <christianparker512@gmail.com>
2021-07-20 15:06:21 -0500

    onclick change name

--- src/App.js
+++ src/App.js
@@ -1,69 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 import Card from './Card';
-import faker from 'faker';
-
+import faker from 'faker'
 function App() {
-    const [cards, setCards] = useState([
-        {
-            id: "Bob 1",
-            name: 'Brad Duncan',
-            title: 'Project Manager',
-            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ZZrR44fHNgg2JS-Tarb6m6Oq1dW_w_5AMQ&usqp=CAU'
-        },
-        {
-            id: 'Bob 2',
-            name: 'Rob Wells',
-            title: 'Tire Inspector',
-            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ZZrR44fHNgg2JS-Tarb6m6Oq1dW_w_5AMQ&usqp=CAU'
-        },
-        {
-            id: 'Bob 3',
-            name: 'Lamar Weddell',
-            title: 'Caterer',
-            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ZZrR44fHNgg2JS-Tarb6m6Oq1dW_w_5AMQ&usqp=CAU'
-        }
-    ])
-    const [showCard, setShowCard] = useState(true)
-    const toggleShowCard = () => setShowCard(!showCard)
-    const deleteCardHandler = (cardIndex) => {
-        const cards_copy = [...cards]
-        cards_copy.splice(cardIndex, 1)
-        console.log('cards_copy', cards_copy)
-        console.log('cards', cards)
-        setCards(cards_copy)
-
-    }
-    const changeNameHandler = (event, id) => {
-        //1. which card
-        const cardIndex = cards.findIndex(card => card.id == id)
-        //2. make a copy of the cards
-        const cards_copy = [...cards]
-        //3. change the name of the specific card
-        cards_copy[cardIndex].name = event.target.value
-        //4. set the cards with the latest version of card copy
-        setCards(cards_copy)
-    }
-    const buttonStyle = {
-        backgroundColor: null
-    }
-    if (cards.length < 3) buttonStyle.backgroundColor = 'lightpink';
-    if (cards.length < 2) buttonStyle.backgroundColor = 'red';
-    const cardsMarkup = showCard && (
-        cards.map((card, index) => <Card
-            avatar={card.avatar}
-            name={card.name}
-            title={card.title}
-            key={card.id}
-            onDelete={() => deleteCardHandler(index)}
-            onChangeName={(event) => changeNameHandler(event, card.id)}
-        />)
+    const [name, setName] = useState('Alan Smith')
+    const buttonsMarkup = (
+        <div>
+            <button className="button button2">YES</button>
+            <button className="button button3">NO</button>
+        </div>
     )
-
+    const changeNameHandler = (name) => {
+        setName(name)
+    }
     return (
         <div className="App">
-            <button className="button" style={buttonStyle} onClick={toggleShowCard}>Toggle show/hide</button>
-            {cardsMarkup}
+            <button className="button" onClick={() => changeNameHandler('John Doe')}>Change Name</button>
+            <Card
+                avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1ZZrR44fHNgg2JS-Tarb6m6Oq1dW_w_5AMQ&usqp=CAU'
+                name={name}
+                title='International Creative Administrator'
+                onChangeName={() => changeNameHandler('Christian Parker')}
+            >{buttonsMarkup}
+            </Card>
+
+
+
         </div>
     );
 }
